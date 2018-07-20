986314ecf8e31718202eb54c8895804e0a9a6dbe
tphdev <tphdevdrop@gmail.com>
2018-07-20 18:58:06 -0500

    adding readme modifying tests

--- assignment-files/ex-10-merge.js
+++ assignment-files/ex-10-merge.js
@@ -11,48 +11,70 @@
 
 
 
+// ++ YOUR CODE HERE
 
 
 
 
 
 
-//*~*~*-*~*~*~*~*~ Don't Touch *~*~*~*~*~*~*~*~*~*~*~
+
+
+
+
+// *~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*
+// *~*~*~*~*~*~*~* Tests (Don't Touch) *~*~*~*~*~*~*~*~*
+// *~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*
+
+console.group('ex-10');
+  console.log('%cFunction: merge', 'background-color: green; color: white')
+console.groupEnd();
+
+/* ----------------------- TEST-1  ----------------------- */
+// merge(obj1, obj2)
+//  should return:
+//   {
+//     accountActive: true,
+//     age: 33,
+//     balance: 38832,
+//     id: 6288,
+//     name: "Shaman Tilroy"
+//    }
+/* ------------------------------------------------------ */
+console.log('TEST-1');
+
 var obj1 = {name: "Shaman Tilroy", age: 33, id: 6288}
 var obj2 = {id: 6288, balance: 38832, accountActive: true }
 
 var merged = merge(obj1, obj2)
 
-console.log("[1] Should return 'Shaman Tilroy'");
 console.assert(merged.name === "Shaman Tilroy")
-console.log("============================================");
-console.log();
-
-console.log("[2] Should return 33");
 console.assert(merged.age === 33)
-console.log("============================================");
-console.log();
-
-console.log("[3] Should return 6288");
 console.assert(merged.id === 6288)
-console.log("============================================");
-console.log();
-
-console.log("[4] Should return 38832");
 console.assert(merged.balance === 38832)
-console.log("============================================");
-console.log();
-
-console.log("[5] Should return true");
 console.assert(merged.accountActive === true)
-console.log("============================================");
-console.log();
 
-// -------------------------------
+
+
+/* ----------------------- TEST-2  ----------------------- */
+// merge(obj3, obj4)
+//  should return:
+// {
+//   album: "Wishy Washy",
+//   ​artist: "The Riverboat",
+//   quantity: 10,
+//   released: 1980,
+//   title: "How to Wear a Bowtie"
+// }
+/* ------------------------------------------------------ */
+console.log('TEST-2');
+
+
 var obj3 = {title: "How to Wear a Bowtie", artist: "Showtime", released: 1984 }
 var obj4 = {artist: "The Riverboat", released: 1980, album: "Wishy Washy", quantity: 10 }
 
 var merged2 = merge(obj3, obj4)
+console.log(merged2);
 
 console.assert( merged2.artist === "The Riverboat" )
 console.assert( merged2.released === 1980 )
